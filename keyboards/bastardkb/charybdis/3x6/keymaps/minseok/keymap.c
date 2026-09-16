e075df5449975114b3548f630969c7270759bc18
KimMinSeok-4856 <KimMinSeok-4856@users.noreply.github.com>
2026-09-16 15:21:50 +0900

    fix(keymap): update keycodes to modern QMK syntax and define DRGSCRL

--- keyboards/bastardkb/charybdis/3x6/keymaps/minseok/keymap.c
+++ keyboards/bastardkb/charybdis/3x6/keymaps/minseok/keymap.c
@@ -10,6 +10,8 @@
 
 #include QMK_KEYBOARD_H
 
+#define DRGSCRL QK_KB_6
+
 // Tap Dance indexes
 enum tap_dance_indexes {
     TD_0 = 0,
@@ -26,18 +28,18 @@ enum tap_dance_indexes {
 };
 
 // Tap Dance definitions matching dd.vil
-qk_tap_dance_action_t tap_dance_actions[] = {
+tap_dance_action_t tap_dance_actions[] = {
     [TD_0]  = ACTION_TAP_DANCE_DOUBLE(KC_N, KC_B),
     [TD_1]  = ACTION_TAP_DANCE_DOUBLE(KC_Z, LCTL(KC_Z)),
     [TD_2]  = ACTION_TAP_DANCE_DOUBLE(KC_X, LCTL(KC_X)),
     [TD_3]  = ACTION_TAP_DANCE_DOUBLE(KC_C, LCTL(KC_C)),
     [TD_4]  = ACTION_TAP_DANCE_DOUBLE(KC_V, LCTL(KC_V)),
     [TD_5]  = ACTION_TAP_DANCE_DOUBLE(KC_E, LGUI(KC_E)),
-    [TD_6]  = ACTION_TAP_DANCE_DOUBLE(KC_RALT, KC_RCTRL),
-    [TD_7]  = ACTION_TAP_DANCE_DOUBLE(KC_MINUS, KC_EQUAL),
-    [TD_8]  = ACTION_TAP_DANCE_DOUBLE(KC_BSPACE, KC_DELETE),
-    [TD_9]  = ACTION_TAP_DANCE_DOUBLE(KC_SLASH, KC_BSLASH),
-    [TD_10] = ACTION_TAP_DANCE_DOUBLE(KC_LBRACKET, KC_RBRACKET),
+    [TD_6]  = ACTION_TAP_DANCE_DOUBLE(KC_RALT, KC_RCTL),
+    [TD_7]  = ACTION_TAP_DANCE_DOUBLE(KC_MINS, KC_EQL),
+    [TD_8]  = ACTION_TAP_DANCE_DOUBLE(KC_BSPC, KC_DEL),
+    [TD_9]  = ACTION_TAP_DANCE_DOUBLE(KC_SLSH, KC_BSLS),
+    [TD_10] = ACTION_TAP_DANCE_DOUBLE(KC_LBRC, KC_RBRC),
 };
 
 // Keymaps
@@ -46,31 +48,31 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // Row 0
        KC_TAB, KC_Q, KC_W, TD(TD_5), KC_R, KC_T,    KC_Y, KC_U, KC_I, KC_O, KC_P, TD(TD_10),
   // Row 1
-       KC_LCTRL, KC_A, KC_S, KC_D, LT(1, KC_F), KC_G,    KC_H, KC_J, KC_K, KC_L, KC_SCOLON, KC_QUOTE,
+       KC_LCTL, KC_A, KC_S, KC_D, LT(1, KC_F), KC_G,    KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOTE,
   // Row 2
        KC_LALT, TD(TD_1), TD(TD_2), TD(TD_3), TD(TD_4), KC_B,    KC_N, KC_M, KC_COMMA, KC_DOT, TD(TD_9), TD(TD_6),
   // Thumb keys (Left: 3, Right: 2)
-                                  KC_LSHIFT, KC_ENTER, MO(3),    TD(TD_8), LT(2, KC_SPACE)
+                                  KC_LSFT, KC_ENTER, MO(3),    TD(TD_8), LT(2, KC_SPC)
   ),
 
   [1] = LAYOUT(
   // Row 0
        KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6,    KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12,
   // Row 1
-       _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    KC_HOME, XXXXXXX, KC_UP, XXXXXXX, KC_PGUP, KC_PSCREEN,
+       _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    KC_HOME, XXXXXXX, KC_UP, XXXXXXX, KC_PGUP, KC_PSCR,
   // Row 2
-       _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    KC_END, KC_LEFT, KC_DOWN, KC_RIGHT, KC_PGDOWN, XXXXXXX,
+       _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    KC_END, KC_LEFT, KC_DOWN, KC_RIGHT, KC_PGDN, XXXXXXX,
   // Thumb keys (Left: 3, Right: 2)
                                   _______, _______, _______,    _______, _______
   ),
 
   [2] = LAYOUT(
   // Row 0
-       KC_GESC, KC_1, KC_2, KC_3, KC_4, KC_5,    KC_6, KC_7, KC_8, KC_9, KC_0, TD(TD_7),
+       QK_GRAVE_ESCAPE, KC_1, KC_2, KC_3, KC_4, KC_5,    KC_6, KC_7, KC_8, KC_9, KC_0, TD(TD_7),
   // Row 1
        _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX, KC_KP_4, KC_KP_5, KC_KP_6, XXXXXXX, XXXXXXX,
   // Row 2
-       _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    KC_KP_0, KC_KP_1, KC_KP_2, KC_KP_3, KC_KP_DOT, KC_NUMLOCK,
+       _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    KC_KP_0, KC_KP_1, KC_KP_2, KC_KP_3, KC_KP_DOT, KC_NUM_LOCK,
   // Thumb keys (Left: 3, Right: 2)
                                   _______, _______, _______,    _______, _______
   ),
@@ -81,7 +83,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // Row 1
        _______, XXXXXXX, XXXXXXX, DRGSCRL, XXXXXXX, XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   // Row 2
-       _______, XXXXXXX, KC_BTN1, KC_BTN3, KC_BTN2, XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+       _______, XXXXXXX, MS_BTN1, MS_BTN3, MS_BTN2, XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   // Thumb keys (Left: 3, Right: 2)
                                   _______, _______, _______,    _______, _______
   )
