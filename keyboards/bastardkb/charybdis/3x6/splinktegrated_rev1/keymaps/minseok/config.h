4fd79504b85d216ea0acdcefa5832b68674cbeb4
김민석 (MinSeok Kim) <minseok.kim@raonrobot.com>
2026-09-17 17:44:25 +0900

    Lower trackball mouse pointer sensitivity to 800 CPI

--- keyboards/bastardkb/charybdis/3x6/splinktegrated_rev1/keymaps/minseok/config.h
+++ keyboards/bastardkb/charybdis/3x6/splinktegrated_rev1/keymaps/minseok/config.h
@@ -3,6 +3,10 @@
 #define SPLIT_LAYER_STATE_ENABLE
 #define SPLIT_LED_STATE_ENABLE
 
+// Trackball Mouse Pointer Sensitivity (기본 1600 -> 800으로 절반 감속)
+#define PMW33XX_CPI 800
+#define CHARYBDIS_MINIMUM_DEFAULT_DPI 800
+
 // Drag-scroll sensitivity and direction
 #define CHARYBDIS_DRAGSCROLL_BUFFER_SIZE 20
 #define CHARYBDIS_DRAGSCROLL_REVERSE_Y
